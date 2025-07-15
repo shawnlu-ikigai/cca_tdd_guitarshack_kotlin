785db79ae7e17d65ca3d2789cac528beb46e0b9f
Jason Gorman <jason.gorman@codemanship.com>
2025-07-15 10:35:05 +0100

    Changed pom.xml to have Maven include dependencies in JAR on package, and added shipping.bat to make testing in the terminal easier

---
+++ shipping.bat
@@ -0,0 +1,4 @@
+REM You can use this Windows batch file (or adapt for Bash etc) to test the Shipping class in the terminal.
+REM Remember to mvn package first if you've changed the code
+
+java -jar target/guitarshack-1.0-SNAPSHOT.jar %1 %2
